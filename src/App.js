e96e5082af503eec075ffacbb9f1c749f9cf2cca
Regbmiller <40448656+Regbmiller@users.noreply.github.com>
2020-01-16 18:25:58 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Router as BrowserRouter, Route, Link } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+
+const App = () => {  
+  
+  return (
+    <div className="App">
+      <nav>
+        <h1 className="store-header">React Router Mini</h1>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+          <Link to="/Contact">Contact</Link>
+        </div>
+      </nav>
+    
+      <Route exact path="/" component={Home} />
+      <Route exact path="/About" component={About} />
+      <Route exact path="/Contact" component={Contact} />
+    </div>
+  );
+};
 
 export default App;
+  
